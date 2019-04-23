cbbefbd6341c3e53f769aff7f8f7fd240ed58b36
ANKAIAH.NANDYALA <ANKAIAH.NANDYALA@LAPTOP-FV63L6VC>
2019-04-23 13:35:12 +0530

    3 rd changes

--- src/com/app/Shivani.java
+++ src/com/app/Shivani.java
@@ -36,6 +36,7 @@ public class Shivani extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		doGet(request, response);
 	}
